23de21bda5e5ba97d6f83340a711fb6a72965698
CSSinghNet <chandrashekharsingh25@gmail.com>
2021-02-22 19:38:40 +0530

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,30 @@
+import { Component, VERSION } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { MessageService } from './_services/message.service';
+
+@Component({
+  selector: 'app',
+  templateUrl: './app.component.html',
+  styleUrls: [ './app.component.css' ]
+})
+export class AppComponent  {
+   messages: any[] = [];
+    subscription: Subscription;
+
+    constructor(private messageService: MessageService) {
+        // subscribe to home component messages
+        this.subscription = this.messageService.onMessage().subscribe(message => {
+            if (message) {
+                this.messages.push(message);
+            } else {
+                // clear messages when empty message received
+                this.messages = [];
+            }
+        });
+    }
+
+    ngOnDestroy() {
+        // unsubscribe to ensure no memory leaks
+        this.subscription.unsubscribe();
+    }
+}
